786bd1d1494ffc03c293209669433e724b15640c
yssrexe <elyasiniyassir40@gmail.com>
2024-05-03 18:03:02 +0100

    task 01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# install flask
+# install_flask.pp
 
-package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.2',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
