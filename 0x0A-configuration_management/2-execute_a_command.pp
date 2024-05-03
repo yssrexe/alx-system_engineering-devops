8bdb80666e4becd5c5a89a6e1a5e57122e33e456
yssrexe <elyasiniyassir40@gmail.com>
2024-05-03 18:22:56 +0100

    task 02

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,14 @@
 # create a manifest that kills a process named killmenow
-exec { killmenow:
-    command => '/bin/pkill -f "killmenow"',
+exec { 'killmenow':
+  command     => '/usr/bin/pkill killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+  subscribe   => File['killmenow_script'],
+}
+
+file { 'killmenow_script':
+  path    => '/path/to/killmenow',
+  ensure  => 'present',
+  content => '#!/bin/bash\nwhile [[ true ]]; do\nsleep 2\ndone\n',
+  mode    => '0755',
 }
