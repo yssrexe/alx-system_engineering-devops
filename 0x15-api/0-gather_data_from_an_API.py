787178414cc640811f2743f94495b92c7d57c6dc
yssrexe <elyasiniyassir400@gmail.com>
2024-06-03 17:32:44 +0100

    task 01

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-vfgfgfdgd
+"""This script displays user's TODO list using {JSON} Placeholder REST API"""
+
+
+def main():
+    """Returns information about TODO list progress of given employee ID"""
+    import requests
+    import sys
+
+    user_id = int(sys.argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        '{}'.format(user_id))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos' +
+                         '?userId={}'.format(user_id))
+
+    done = [task for task in todos.json() if task.get('completed', False)]
+    print("Employee {} is done with tasks".format(user.json().get('name')) +
+          "({}/{}):".format(len(done), len(todos.json())))
+    for task in done:
+        print("\t {}".format(task.get('title')))
+
+
+if __name__ == '__main__':
+    main()
